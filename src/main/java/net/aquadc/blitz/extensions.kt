04f0bc0dd64f7dca0e5f46f9806282761a7865d6
Miha_x64 <mihlo1996@rambler.ru>
2017-03-29 00:18:50 +0300

    Kotlin extensions and operators

---
+++ src/main/java/net/aquadc/blitz/extensions.kt
@@ -0,0 +1,138 @@
+@file:Suppress("NOTHING_TO_INLINE")
+@file:JvmName("LongSets")
+
+package net.aquadc.blitz
+
+import net.aquadc.blitz.impl.ImmutableLongTreeSet
+import net.aquadc.blitz.impl.MutableLongHashSet
+
+/**
+ * Created by miha on 28.03.17
+ */
+
+// factory
+
+inline fun mutableLongSetOf(): MutableLongSet = MutableLongHashSet()
+inline fun mutableLongSetOf(vararg elements: Long): MutableLongSet = MutableLongHashSet(elements)
+
+inline fun immutableLongSetOf(): ImmutableLongSet = ImmutableLongTreeSet.empty()
+inline fun immutableLongSetOf(element: Long): ImmutableLongSet = ImmutableLongTreeSet.singleton(element)
+inline fun immutableLongSetOf(vararg elements: Long): ImmutableLongSet = ImmutableLongTreeSet.from(elements)
+
+inline fun Long.toLongSet(): ImmutableLongSet = ImmutableLongTreeSet.singleton(this)
+inline fun LongArray.toLongSet(): ImmutableLongSet = ImmutableLongTreeSet.from(this)
+
+// common
+
+// already picked up by Kotlin
+//inline operator fun LongSet.contains(l: Long) = contains(l)
+//inline operator fun LongSet.iterator()
+
+inline operator fun LongSet.contains(l: LongArray) = containsAll(l)
+inline operator fun LongSet.contains(l: LongSet) = containsAll(l)
+
+inline fun LongSet.single(): Long = when (size()) {
+    0 -> throw NoSuchElementException("Collection is empty.")
+    1 -> if (this is OrderedLongSet) get(0) else iterator().next()
+    else -> throw IllegalArgumentException("Collection has more than one element.")
+}
+
+inline fun LongSet.singleOrDefault(default: Long) = when (size()) {
+    1 -> if (this is OrderedLongSet) get(0) else iterator().next()
+    else -> default
+}
+
+inline fun LongSet.filtered(predicate: (Long) -> Boolean): LongSet = filterTo(MutableLongHashSet(), predicate)
+inline fun LongSet.filteredNot(predicate: (Long) -> Boolean): LongSet = filterNotTo(MutableLongHashSet(), predicate)
+
+inline fun LongSet.filterTo(destination: MutableLongSet, predicate: (Long) -> Boolean): MutableLongSet {
+    for (element in this) if (predicate(element)) destination.add(element)
+    return destination
+}
+inline fun LongSet.filterNotTo(destination: MutableLongSet, predicate: (Long) -> Boolean): MutableLongSet {
+    for (element in this) if (!predicate(element)) destination.add(element)
+    return destination
+}
+
+inline fun LongSet.asSortedArray(): LongArray = copyToArray().also { java.util.Arrays.sort(it) }
+
+inline fun LongSet.forEach(code: (Long)->Unit) {
+    val itr = iterator()
+    while (itr.hasNext()) code(itr.next())
+}
+
+inline fun <T : LongSet> T.onEach(code: (Long)->Unit): T = apply {
+    val itr = iterator()
+    while (itr.hasNext()) code(itr.next())
+}
+
+inline fun <T> Collection<T>.mapToLongs(mapToLong: (T) -> Long) =
+        MutableLongHashSet(size).also { set ->
+            forEach { t -> set.add(mapToLong(t)) }
+        }
+
+inline fun LongSet.joinToString(
+        separator: CharSequence = ", ",
+        prefix: CharSequence = "",
+        postfix: CharSequence = "",
+        limit: Int = -1,
+        truncated: CharSequence = "..."
+): String {
+    return joinTo(StringBuilder(), separator, prefix, postfix, limit, truncated).toString()
+}
+
+fun <A : Appendable> LongSet.joinTo(
+        buffer: A,
+        separator: CharSequence = ", ",
+        prefix: CharSequence = "",
+        postfix: CharSequence = "",
+        limit: Int = -1,
+        truncated: CharSequence = "..."
+): A {
+    buffer.append(prefix)
+    var count = 0
+    for (element in this) {
+        if (++count > 1) buffer.append(separator)
+        if (limit < 0 || count <= limit) buffer.append(element.toString()) else break
+    }
+    if (limit >= 0 && count > limit) buffer.append(truncated)
+    buffer.append(postfix)
+    return buffer
+}
+
+inline fun LongSet.average(): Double {
+    var sum: Double = 0.0
+    var count: Int = 0
+    for (element in this) {
+        sum += element
+        count += 1
+    }
+    if (count == 0) throw NoSuchElementException()
+    return sum / count
+}
+
+inline fun LongSet.sum(): Long {
+    var sum: Long = 0L
+    for (element in this) sum += element
+    return sum
+}
+
+// mutable
+
+inline operator fun MutableLongSet.plusAssign(l: Long) { add(l) }
+inline operator fun MutableLongSet.plusAssign(l: LongArray) { addAll(l) }
+inline operator fun MutableLongSet.plusAssign(l: LongSet) { addAll(l) }
+
+inline operator fun MutableLongSet.minusAssign(l: Long) { remove(l) }
+inline operator fun MutableLongSet.minusAssign(l: LongArray) { removeAll(l) }
+inline operator fun MutableLongSet.minusAssign(l: LongSet) { removeAll(l) }
+
+// immutable
+
+inline operator fun ImmutableLongSet.plus(l: Long): ImmutableLongSet = with(l)
+inline operator fun ImmutableLongSet.plus(l: LongArray): ImmutableLongSet = withAll(l)
+inline operator fun ImmutableLongSet.plus(l: LongSet): ImmutableLongSet = withAll(l)
+
+inline operator fun ImmutableLongSet.minus(l: Long): ImmutableLongSet = without(l)
+inline operator fun ImmutableLongSet.minus(l: LongArray): ImmutableLongSet = withoutAll(l)
+inline operator fun ImmutableLongSet.minus(l: LongSet): ImmutableLongSet = withoutAll(l)
---
+++ src/test/java/net/aquadc/blitz/ExtensionsTest.kt
@@ -0,0 +1,59 @@
+package net.aquadc.blitz
+
+import net.aquadc.blitz.impl.ImmutableLongTreeSet
+import net.aquadc.blitz.impl.MutableLongHashSet
+import org.junit.Assert.assertArrayEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Created by miha on 28.03.17
+ */
+
+class ExtensionsText {
+
+    @Test fun singleTest() {
+        assertEquals(1, immutableLongSetOf(1).single())
+        assertEquals(1, immutableLongSetOf(1).singleOrDefault(2))
+        assertEquals(2, immutableLongSetOf().singleOrDefault(2))
+    }
+
+    @Test(expected = NoSuchElementException::class) fun singleEmptyTest() {
+        immutableLongSetOf().single()
+    }
+
+    @Test(expected = IllegalArgumentException::class) fun singleFullTest() {
+        immutableLongSetOf(1, 2).single()
+    }
+
+    @Test fun testFilter() {
+        assertEquals(immutableLongSetOf(0, 1, 2, 3, 4, 5, 6, 7).filtered { it > 3 }, immutableLongSetOf(4, 5, 6, 7))
+        assertEquals(immutableLongSetOf(0, 1, 2, 3, 4, 5, 6, 7).filteredNot { it > 3 }, immutableLongSetOf(0, 1, 2, 3))
+    }
+
+    @Test fun testSorting() {
+        assertArrayEquals(mutableLongSetOf(1, 26, 34, 72, 96, 111).asSortedArray(), longArrayOf(1, 26, 34, 72, 96, 111))
+    }
+
+    @Test fun testJoining() {
+        assertEquals(immutableLongSetOf(0, 1, 2, 3, 4, 5, 6, 7).joinToString(limit = 3), "0, 1, 2, ...")
+    }
+
+    @Test fun testAvgAndSum() {
+        assertEquals(immutableLongSetOf(0, 1, 2, 3).average(), 1.5, 0.00000000001)
+        assertEquals(immutableLongSetOf(0, 1, 2, 3).sum(), 6)
+    }
+
+    @Test fun testOperators() {
+        val set = mutableLongSetOf()
+        set += 1
+        set += 3
+        assertArrayEquals(longArrayOf(1, 3), set.asSortedArray())
+        set -= 3
+        assertArrayEquals(longArrayOf(1), set.asSortedArray())
+
+        assertEquals(immutableLongSetOf(4), immutableLongSetOf() + 4)
+        assertEquals(immutableLongSetOf(), immutableLongSetOf(4) - 4)
+    }
+
+}
